7ecdf97d44a2816075e307f10975917eba0fca24
unknown <glimarecuperacao@gmail.com>
2023-02-16 20:45:02 -0300

    Json and Json Server implementation

---
+++ src/assets/js/script.js
@@ -0,0 +1,57 @@
+const body = document.getElementById("body")
+const burguer = document.querySelector(".burguer")
+const nav = document.querySelector(".nav_header")
+const isadora = document.querySelector(".box_noticia")
+const navLinks = document.querySelectorAll('.nav_header a')
+
+
+const navSlide = () => {
+
+    //HAMBURGUER MENU
+    burguer.addEventListener("click", () => {
+        //ANIMAÇÃO HAMBURGUER
+        nav.classList.toggle("nav_ativa")
+
+        //ANIMAÇÃO LINKS
+        navLinks.forEach((link, index)=> {
+            if(link.style.animation){
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navHeaderFade 0.5s ease forwards ${index / 7 + 0.4}s`
+            }
+        })
+        // ANIMAÇÃO X DO BURGUER
+        burguer.classList.toggle('toggle')
+
+
+    })
+}
+
+const navSome = () => {
+    navLinks.forEach((link) => {
+        link.addEventListener("click", () => {
+            nav.classList.toggle('nav_ativa')
+            burguer.classList.toggle('toggle')
+            navLinks.forEach((link, index)=> {
+                if(link.style.animation){
+                    link.style.animation = ''
+                }
+                else {
+                    link.style.animation = `navHeaderFade 0.5s ease forwards ${index / 7 + 0.4}s`
+                }
+            })
+        })
+    })
+}
+
+// const abreNoticia = () => {
+//     isadora.addEventListener("click", () => {
+//         isadora.classList.add('isadora_img_clicked')
+//     })
+// }
+
+
+navSlide()
+navSome()
+// abreNoticia ()
